c83966c0a98cc26796bbc9a6acd79a1fae7adb44
jidong <jee599@naver.com>
2018-06-14 00:15:45 +0900

    d
    d

--- Team/Main.py
+++ Team/Main.py
@@ -2,11 +2,12 @@
 from readdata import *
 from tkinter import *
 from tkinter import font
+from xml.etree.ElementTree import parse
 
 import xml.etree.ElementTree as ET
 
-Data1 = LoadXML1()
-Data2 = LoadXML2()
+doc = parse("Data1.xml")
+Data1 = doc.getroot()
 
 window = Tk()
 window.title("휴게소 대표 음식")
@@ -19,14 +20,22 @@ def CreateTitleLabel():
     ltitle.pack()
     ltitle.place(x=20, y=30)
 
-def CreateSearchListBox():
-    global SearchListBox
+def CreateAddLabel():
+    global AddLabel
     TempFont = font.Font(window, size=15,weight='bold', family = 'Consolas')
-    SearchListBox = Entry(window, font = TempFont, width = 26, borderwidth = 12, relief = 'ridge')
+    AddLabel = Entry(window, font = TempFont, width = 26, borderwidth = 12, relief = 'ridge')
 
-    SearchListBox.pack()
-    SearchListBox.place(x=10, y=80)
+    AddLabel.pack()
+    AddLabel.place(x=10, y=80)
 
+def CreateDelLabel():
+    global DelLabel
+    TempFont = font.Font(window, size=15, weight='bold', family='Consolas')
+    DelLabel = Entry(window, font=TempFont, width=26, borderwidth=12, relief='ridge')
+    DelLabel.pack()
+    DelLabel.place(x=10, y=200)
+def CreateDelButton():
+    
 def CreateInputLabel():
     global InputLabel
     TempFont = font.Font(window, size = 15, weight = 'bold', family = 'Consolas')
@@ -42,7 +51,7 @@ def CreateSearchButton():
 
 def CreateAddButton():
     TempFont = font.Font(window, size=12, weight='bold', family='Consolas')
-    AddButton = Button(window, font = TempFont, borderwidth = 10, text = "추가", command=SearchButtonAction)
+    AddButton = Button(window, font = TempFont, borderwidth = 10, text = "추가", command=AddButtonAction)
     AddButton.pack()
     AddButton.place(x=330, y=80)
 
@@ -56,11 +65,10 @@ def CreateRenderText():
     RenderText = Text(window, width = 49, height = 27, borderwidth = 8, relief = 'ridge',
                       yscrollcommand = RenderTextScrollbar.set)
     RenderText.pack()
-    RenderText.place(x=10,y=195)
+    RenderText.place(x=10,y=250)
     RenderTextScrollbar.config(command=RenderText.yview)
     RenderTextScrollbar.pack(side = RIGHT, fill = Y)
     RenderText.configure(state='disabled')
-
 
 def CreatePrintListButton():
     TempFont = font.Font(window, size=12, weight='bold', family='Consolas')
@@ -68,8 +76,28 @@ def CreatePrintListButton():
     SearchButton.pack()
     SearchButton.place(x=290, y= 20)
 
+def DelButtonAction():
+    global DelLabel, RenderText, window
+    RenderText.configure(state='normal')
+    RenderText.delete(0.0, END)
+
+    keyword = str(DelLabel.get())
+
+    RenderText.configure(state='disabled')
+    DelLabel.delete(0, END)
+
+def AddButtonAction():
+    global AddLabel, RenderText, window
+    RenderText.configure(state='normal')
+    RenderText.delete(0.0, END)
+
+    keyword = str(AddLabel.get())
+
+    RenderText.configure(state = 'disabled')
+    AddLabel.delete(0,END)
+
 def SearchButtonAction():
-    global SearchListBox, InputLabel, RenderText,window
+    global InputLabel, RenderText,window
     RenderText.configure(state='normal')
     RenderText.delete(0.0, END)
 
@@ -79,7 +107,6 @@ def SearchButtonAction():
 
     SearchLibName(keyword)
     SearchLibAddress(keyword)
-    DeletePrice(keyword)
 
     #RenderText.insert(INSERT, InputLabel.get())
 
@@ -93,7 +120,6 @@ def PrintList():
     RenderText.configure(state='normal')
     RenderText.delete(0.0, END)
 
-    ET.dump(Data1)
     i=0
 
     for location in Data1.findall("list"):
@@ -129,48 +155,18 @@ def PrintList():
 
         i += 1
 
-    if (i >= 99):
-        for location in Data2.findall("list"):
-            RenderText.insert(INSERT, "[")
-            RenderText.insert(INSERT, i + 1)
-            RenderText.insert(INSERT, "]")
-
-            RenderText.insert(INSERT, chr(10))
-            RenderText.insert(INSERT, "휴게소명    : ")
-            RenderText.insert(INSERT, location.findtext("serviceAreaName"))
-            RenderText.insert(INSERT, "휴게소 ")
-            RenderText.insert(INSERT, chr(10))
-            try:
-                RenderText.insert(INSERT, "대표음식 : ")
-                RenderText.insert(INSERT, location.findtext("batchMenu"))
-                RenderText.insert(INSERT, chr(10))
-            except:
-                RenderText.insert(INSERT, chr(10))
-                pass
-            try:
-                RenderText.insert(INSERT, "가격    : ")
-                RenderText.insert(INSERT, location.findtext("salePrice"))
-                RenderText.insert(INSERT, chr(10))
-            except:
-                RenderText.insert(INSERT, chr(10))
-                pass
-            RenderText.insert(INSERT, "위치    : ")
-            RenderText.insert(INSERT, location.findtext("routeName"))
-            RenderText.insert(INSERT, chr(10))
-            RenderText.insert(INSERT, "방향    : ")
-            RenderText.insert(INSERT, location.findtext("direction"))
-            RenderText.insert(INSERT, chr(10))
-
-            i += 1
 
     RenderText.configure(state='disabled')
 
-def DeletePrice(keyword):
+def DeleteName(keyword):
     global Data1, RenderText
 
     for location in Data1.findall("list"):
-        if keyword in location.findtext("salePrice"):
+        if keyword in location.findtext("serviceAreaName"):
             Data1.remove(location)
+
+    doc.write("Data1.xml", encoding="utf-8", xml_declaration=True)
+
 
 def SearchPrice(keyword):
     global Data1, RenderText
@@ -253,9 +249,9 @@ def SearchLibAddress(keyword):
 
             i += 1
 
-
 CreateTitleLabel()
-CreateSearchListBox()
+CreateAddLabel()
+CreateDelLabel()
 CreateInputLabel()
 CreateSearchButton()
 CreateAddButton()
